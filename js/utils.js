36c94893d067d5d5cacd0880f656ced2010f826c
aaron.dewberry <aaron.dewberry@coxautoinc.com>
2019-11-11 14:16:52 -0600

    adding ingredient form filtering

--- js/utils.js
+++ js/utils.js
@@ -10,6 +10,8 @@ export const getData = (url, selector) => {
         .then(response => response.json())
         .then(data => {
             const outerContainer = document.querySelector(selector);
+            outerContainer.innerHTML = ''; // Clear container
+
             const section = document.createElement('section');
             outerContainer.appendChild(section);
             section.classList.add('container');
@@ -21,49 +23,49 @@ export const getData = (url, selector) => {
 
             const sortedRecipes = sortRecipeByTitle(data.results);
 
-            console.log(data);
-
             sortedRecipes.forEach(recipe => {
                 const { title, ingredients, href, thumbnail } = recipe;
 
                 const card = document.createElement('div');
                 card.classList.add('card');
 
-                const h2 = document.createElement('h2');
-                h2.textContent = title;
+                const cardTitle = document.createElement('h2');
+                cardTitle.classList.add('card-title');
+                cardTitle.textContent = title;
 
-                const p = document.createElement('p');
-                const ingredientsArray = ingredients.trim().split(',');
+                const cardBody = document.createElement('div');
+                cardBody.classList.add('card-body', 'm-0', 'pt-0', 'pb-5', 'px-5');
 
                 if (thumbnail) {
                     fetch(proxyUrl + thumbnail, {'Retry-After': 3600})
                         .then(response => {
                             if (response.ok) {
-                                p.insertAdjacentHTML('afterbegin', `<img src="${thumbnail}" class="d-block mb-3 mx-auto" alt="${title}"/>`);
+                                cardBody.insertAdjacentHTML('afterbegin', `<img src="${thumbnail}" class="d-block mb-3 mx-auto" alt="${title}"/>`);
                             } else {
-                                p.insertAdjacentHTML('afterbegin', `<img src="/images/utensils-icon.png" class="d-block mb-3 mx-auto" alt="${title}"/>`);
+                                cardBody.insertAdjacentHTML('afterbegin', `<img src="/images/utensils-icon.png" class="d-block mb-3 mx-auto" alt="${title}"/>`);
                             }
                         })
                         .catch(err => console.debug(`Failed to fetch ${thumbnail} due to ${err}`));
                 } else {
-                    p.insertAdjacentHTML('afterbegin', `<img src="/images/utensils-icon.png" class="d-block mb-3 mx-auto" alt="${title}"/>`);
+                    cardBody.insertAdjacentHTML('afterbegin', `<img src="/images/utensils-icon.png" class="d-block mb-3 mx-auto" alt="${title}"/>`);
                 }
 
+                const ingredientsArray = ingredients.trim().split(',');
                 ingredientsArray.map((ingredient, index) => {
-                    const a = document.createElement('a');
-                    a.href = '#';
+                    const ingredientLink = document.createElement('a');
+                    ingredientLink.href = '#';
 
                     ingredient = ingredient.trim();
-                    a.setAttribute('data-ingredient', ingredient);
-                    a.textContent = ingredient;
-                    p.appendChild(a);
+                    ingredientLink.setAttribute('data-ingredient', ingredient);
+                    ingredientLink.textContent = ingredient;
+                    cardBody.appendChild(ingredientLink);
 
                     const lastIngredient = ingredientsArray.length === index + 1;
                     if (!lastIngredient) {
-                        a.insertAdjacentHTML('afterend', ', ');
+                        ingredientLink.insertAdjacentHTML('afterend', ', ');
                     }
 
-                    a.addEventListener('click', event => {
+                    ingredientLink.addEventListener('click', event => {
                         event.preventDefault();
                         const form = document.forms['recipe-fetch-form'];
                         const input = document.createElement('input');
@@ -72,23 +74,25 @@ export const getData = (url, selector) => {
                         input.setAttribute('value', ingredient);
                         form.appendChild(input);
 
-                        //getData('http://www.recipepuppy.com/api/', '#recipe-fetch-list');
+                        const submitEvent = new Event('submit');
+
+                        form.dispatchEvent(submitEvent);
                     });
                 });
 
                 section.appendChild(card);
-                card.appendChild(h2);
-                card.appendChild(p);
+                card.appendChild(cardTitle);
+                card.appendChild(cardBody);
 
                 fetch(proxyUrl + href, { 'Retry-After': 3600 })
                     .then(response => {
                         if (response.ok) {
-                            const a = document.createElement('a');
-                            a.href = href;
-                            a.target = '_blank';
-                            a.classList.add('recipe-link', 'm-0', 'mt-3', 'd-block', 'text-center', 'btn', 'btn-default');
-                            a.textContent = 'View Recipe';
-                            p.appendChild(a);
+                            const recipeLink = document.createElement('a');
+                            recipeLink.href = href;
+                            recipeLink.target = '_blank';
+                            recipeLink.classList.add('card-link', 'm-0', 'mt-3', 'd-block', 'text-center', 'btn', 'btn-default');
+                            recipeLink.textContent = 'View Recipe';
+                            cardBody.appendChild(recipeLink);
                         }
                     })
                     .catch(err => console.debug(`Failed to fetch ${href} due to ${err}`));
